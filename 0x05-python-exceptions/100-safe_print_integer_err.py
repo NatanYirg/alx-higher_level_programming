08a81be57646992df70e3968816606610d6fcee5
NatanYirg <natiyirgish@gmail.com>
2023-03-28 02:42:41 -0700

    commit

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-def raise_exception_msg(message=""):
-    """Raise a NameError exception with a message."""
-    raise NameError(message)
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
